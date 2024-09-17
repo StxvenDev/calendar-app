a9598f8d836eb7b531665f2b67127f917a2539a7
StevenDev <bossiolealbl@gmail.com>
2024-09-17 16:45:14 -0500

    feat: change language to spanish

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,48 +1,34 @@
-import {Calendar, dateFnsLocalizer} from 'react-big-calendar'
+import {Calendar} from 'react-big-calendar'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
-import format from 'date-fns/format'
-import parse from 'date-fns/parse'
-import startOfWeek from 'date-fns/startOfWeek'
-import getDay from 'date-fns/getDay'
-import enUS from 'date-fns/locale/en-US'
-import { Navbar } from '../components/Navbar'
 import { addHours } from 'date-fns'
+import { Navbar } from '../components/Navbar'
+import { getMessagesES, localizer } from '../../helpers'
 
-export const CalendarPage = () => {
 
-    const locales = {
-        'en-US': enUS,
+export const CalendarPage = () => { 
+    const events = [{
+      title:'cumpleaños',
+      notes: 'Hay que comprar mecatos',
+      start : new Date(),
+      end : addHours(new Date(), 2),
+      bgColor: '#fafafa',
+      user: {
+          _id: '123',
+          name: 'fernando'
       }
-      
-      const localizer = dateFnsLocalizer({
-        format,
-        parse,
-        startOfWeek,
-        getDay,
-        locales,
-      });
-
-      const events = [{
-        title:'cumpleaños',
-        notes: 'Hay que comprar mecatos',
-        start : new Date(),
-        end : addHours(new Date(), 2),
-        bgColor: '#fafafa',
-        user: {
-            _id: '123',
-            name: 'fernando'
-        }
-      }]
+    }]
 
   return (
     <>
         <Navbar />
         <Calendar
+            culture='es'
             localizer={localizer}
             events={events}
             startAccessor="start"
             endAccessor="end"
             style={{ height: 'calc(100vh - 64px)' }}
+            messages={getMessagesES()}
         />
     </>
   )
---
+++ src/helpers/calendarLocalizer.js
@@ -0,0 +1,20 @@
+import format from 'date-fns/format'
+import parse from 'date-fns/parse'
+import startOfWeek from 'date-fns/startOfWeek'
+import getDay from 'date-fns/getDay'
+import { dateFnsLocalizer } from 'react-big-calendar'
+import { es } from 'date-fns/locale'
+
+const locales = {
+    'es': es,
+  } 
+ 
+  export const localizer = dateFnsLocalizer({
+    format,
+    parse,
+    startOfWeek,
+    getDay,
+    locales,
+  });
+  
+
