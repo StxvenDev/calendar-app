0d30353cda02272433cceed3f23a05ea292797a9
StevenDev <bossiolealbl@gmail.com>
2024-09-18 17:36:01 -0500

    feat: calendar modal init

---
+++ src/calendar/components/CalendarModal.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import Modal from 'react-modal'
+
+const customStyles = {
+    content: {
+      top: '50%',
+      left: '50%',
+      right: 'auto',
+      bottom: 'auto',
+      marginRight: '-50%',
+      transform: 'translate(-50%, -50%)',
+    },
+  };
+  Modal.setAppElement('#root')
+
+export const CalendarModal = () => {
+
+    const [isModalOpen, setIsModalOpen] = useState(true);
+
+    const onCLoseModal = () => {
+        console.log('cerrando modal');
+        setIsModalOpen(false);
+    }
+
+  return (
+    <Modal
+     isOpen={isModalOpen}
+     onRequestClose={onCLoseModal}
+     style={customStyles}
+     contentLabel="Example Modal"
+     className="modal"
+     overlayClassName="modal-fondo"
+     closeTimeoutMS={200}
+    >
+        <h1>Modal</h1>
+        <hr />
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Delectus .</p>
+    </Modal>
+  )
+}
--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,9 +1,9 @@
+import { useState } from 'react'
 import {Calendar} from 'react-big-calendar'
 import 'react-big-calendar/lib/css/react-big-calendar.css'
 import { addHours } from 'date-fns'
-import { Navbar, CalendarEvent } from '../'
+import { Navbar, CalendarEvent, CalendarModal } from '../'
 import { getMessagesES, localizer } from '../../helpers'
-import { useState } from 'react'
 
 
 export const CalendarPage = () => { 
@@ -65,6 +65,7 @@ export const CalendarPage = () => {
             onSelectEvent={ onSelect }
             onView={ onViewChanged }
         />
+        <CalendarModal />
     </>
   )
 }
